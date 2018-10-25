67db7d596b470e0dc20fab5a62faf750fa976244
James Johnson <nimbus396@hotmail.com>
2018-10-25 14:47:34 -0500

    added various comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Aliens
 Aliens is a space invaders type of game written in JavaScript.
+
+I wrote this game on a bet.  A friend told me Javascript wasn't really capable of running complex games.  We argued for a minute about constructs and I took the bet.  I like old school so, I settled on a space invader look alike.
+
+This is written from scratch and if you change the images, the game will adjust automatically.  The offset is based on the alien image.
--- js/AlienClass.js
+++ js/AlienClass.js
@@ -4,39 +4,41 @@
  *          on a canvas.
  */
 
-// Global Variable for our alien.  We call here so we only load the imageonce.
-this.img = new Image;
+/* Global Variable for our alien.  We call here so we only load the imageonce.
+ * I put these here because these are used in various places and it was easier
+ */ to just make it global.
+  this.img = new Image;
 this.img.src = "images/alien1.png";
 
 // Name the class
 class Alien { 
-    constructor() {
-        this.isAlive=1;
-    }
-    // We need a way to say if the alien is alive or dead
-    set_isAlive(val) {
-        this.isAlive=val;
-    }
+  constructor() {
+    this.isAlive=1;
+  }
+  // We need a way to say if the alien is alive or dead
+  set_isAlive(val) {
+    this.isAlive=val;
+  }
 
-    // We need a way to see if the alien is alive or dead
-    get_isAlive() {
-        return this.isAlive;
-    }
+  // We need a way to see if the alien is alive or dead
+  get_isAlive() {
+    return this.isAlive;
+  }
 
-    // Image Width
-    get_Width() {
-        return img.width;
-    }
+  // Image Width
+  get_Width() {
+    return img.width;
+  }
 
-    // Image height
-    get_Height() {
-        return img.height;
-    }
+  // Image height
+  get_Height() {
+    return img.height;
+  }
 
-    // Get the current canvas and draw a filled rectangle
-    draw(x,y) {
-        var canvas=document.getElementById("gameBoard");
-        var ctx=canvas.getContext("2d");
-        ctx.drawImage(img,x,y);
-    }
+  // Get the current canvas and draw a filled rectangle
+  draw(x,y) {
+    var canvas=document.getElementById("gameBoard");
+    var ctx=canvas.getContext("2d");
+    ctx.drawImage(img,x,y);
+  }
 }
